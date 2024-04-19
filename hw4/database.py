ddee797665521e26631ab0ab079ca59d1422cdf8
igor <dr_kia@mail.ru>
2024-04-19 14:09:26 +0300

    correction sql

--- hw4/database.py
+++ hw4/database.py
@@ -1,39 +1,54 @@
-import random
-
+import pandas as pd
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+#
+# def run_sql(sql):
+#     conn = psycopg2.connect(
+#         database='new_db',
+#         user='postgres',
+#         password='postgres',
+#         host='127.0.0.1',
+#         port=5432
+#     )
+#     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#     cur = conn.cursor()
+#     cur.scalars(sql)
+#     conn.close()
 
-def run_sql(sql):
-    conn = psycopg2.connect(
-        database='new_db',
-        user='postgres',
-        password='postgres',
-        host='127.0.0.1',
-        port=5432
-    )
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.close()
 
+conn = psycopg2.connect(
+    database='new_db',
+    user='postgres',
+    password='postgres',
+    host='127.0.0.1',
+    port=5432
+)
 
-def create_pc_data():
-    """
-    Создание и наполнение таблицы данными о PC
-    :return:
-    """
-    run_sql(
-        'CREATE TABLE PC (code INTEGER, model INTEGER, speed INTEGER, ram INTEGER, '
-        'hd FLOAT, cd VARCHAR(50), price FLOAT)'
-    )
+query = """
+SELECT
+    AGE,
+    GENDER,
+    CHILD_TOTAL,
+    DEPENDANTS,
+    dw.comment as socstatus_work_fl,
+    dp.comment as socstatus_pens_fl,
+    ds.personal_income as personal_income,
+    da.agreement_rk as AGREEMENT_RK,
+    da.target as TARGET,
+    count(dl.id_client) as LOAN_NUM_TOTAL,
+    sum(dcl.closed_fl) as LOAN_NUM_CLOSED
+FROM d_clients
+JOIN d_work dw on dw.id = d_clients.socstatus_work_fl
+JOIN d_pens dp on dp.id = d_clients.socstatus_pens_fl
+JOIN d_salary ds on d_clients.id = ds.id_client
+JOIN d_agreement da on d_clients.id = da.id_client
+JOIN d_loan dl on d_clients.id = dl.id_client
+JOIN d_close_loan dcl on dl.id_loan = dcl.id_loan
+GROUP BY AGE, GENDER, CHILD_TOTAL, DEPENDANTS, dw.comment, dp.comment, ds.personal_income, da.agreement_rk, da.target
+"""
 
-    values = []
-    for code, i in enumerate(range(1121, 1225), start=1):
-        values.append(
-            f'({code}, {i}, {random.randrange(500, 901, 100)},'
-            f' {random.randrange(32, 129, 32)}, {random.randrange(5, 21, 5)},'
-            f' \'{random.randrange(12, 53, 4)}x\', {random.randrange(350, 1001, 50)})'
-        )
-    data = ', '.join(values) + '\n'
-    run_sql(f"INSERT INTO PC VALUES {data}")
+df = pd.read_sql(query, conn)
+
+df.to_csv('client.csv')
+
--- hw4/stmt.sql
+++ hw4/stmt.sql
@@ -9,12 +9,12 @@ SELECT
     da.agreement_rk as AGREEMENT_RK,
     da.target as TARGET,
     count(dl.id_client) as LOAN_NUM_TOTAL,
-    count(dcl.closed_fl) as LOAN_NUM_CLOSED
+    sum(dcl.closed_fl) as LOAN_NUM_CLOSED
 FROM d_clients
 JOIN d_work dw on dw.id = d_clients.socstatus_work_fl
 JOIN d_pens dp on dp.id = d_clients.socstatus_pens_fl
 JOIN d_salary ds on d_clients.id = ds.id_client
 JOIN d_agreement da on d_clients.id = da.id_client
 JOIN d_loan dl on d_clients.id = dl.id_client
-JOIN d_close_loan dcl on dl.id_loan = dcl.id_loan and closed_fl = 1
+JOIN d_close_loan dcl on dl.id_loan = dcl.id_loan
 GROUP BY AGE, GENDER, CHILD_TOTAL, DEPENDANTS, dw.comment, dp.comment, ds.personal_income, da.agreement_rk, da.target
