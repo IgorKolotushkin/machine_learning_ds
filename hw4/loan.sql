c336866379995314d78249341b62cd8a15375d42
igor <dr_kia@mail.ru>
2024-04-19 13:35:09 +0300

    correction sql loan

--- hw4/loan.sql
+++ hw4/loan.sql
@@ -1,3 +1,3 @@
-SELECT loan.id_client, count(loan.id_loan), dcl.closed_fl FROM d_loan as loan
+SELECT loan.id_client, count(loan.id_loan), sum(dcl.closed_fl) as cnt FROM d_loan as loan
                       JOIN d_close_loan dcl on loan.id_loan = dcl.id_loan
-GROUP BY loan.id_client, dcl.closed_fl
+GROUP BY loan.id_client
